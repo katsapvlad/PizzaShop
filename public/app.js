3596dd59473a791166a6a2f2104f56dfa5dfa0b2
katsapvlad <katsapvlad@yandex.by>
2021-01-05 18:20:17 +0300

    Add pizza id

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
-function something () {
+function add(id) {	
+	alert('You add pizza with id:' + id);
+}
+
+function something() {
 	var x = window.localStorage.getItem('aaa');
 	x=x*1+1;
 	window.localStorage.setItem('aaa', x);
