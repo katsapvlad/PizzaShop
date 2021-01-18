79cc1c87af978589c6d7d8fbc99acbef31679b57
katsapvlad <katsapvlad@yandex.by>
2021-01-18 20:57:14 +0300

    Add cart page

--- app.rb
+++ app.rb
@@ -20,5 +20,28 @@ get '/about' do
 end
 
 post '/cart' do
-	erb "This is cart"	
+	orders_input = params[:orders]
+	@items = parse_orders_line orders_input
+	@items.each do |item|
+		item[0]=Product.find(item[0])
+	end
+	erb :cart 	
+end
+
+def parse_orders_line orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+
+		s2 = x.split(/\=/)
+		s3 = s2[0].split(/_/)
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+	return arr
 end
